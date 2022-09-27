90a8549c8fb09ef48e8959851e3ed386b7c5d952
Mario Vega <malbertovega@gmail.com>
2022-09-26 23:29:45 -0500

    addGrade method included

--- student.py
+++ student.py
@@ -1,20 +1,21 @@
 class Student:
-    def __init__(self, name, hours, qpoints):
+    def __init__(self, name, credits, qpoints):
         self.name = name
-        self.hours = float(hours)
+        self.credits = float(credits)
         self.qpoints = float(qpoints)
     
     def getName(self):
         return self.name
     
-    def getHours(self):
-        return self.hours
+    def getCredits(self):
+        return self.credits
 
     def getQpoints(self):
         return self.qpoints
     
     def gpa(self):
-        return self.qpoints / self.hours
+        return self.qpoints / self.credits
 
     def addGrade(self, gradePoint, credits):
         self.qpoints = self.qpoints + (gradePoint * credits)
+        self.credits = self.credits + credits
