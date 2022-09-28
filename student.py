6a96c8003c7c68a7a76627b502b0a5ddf71e82c2
Mario Vega <malbertovega@gmail.com>
2022-09-27 21:56:13 -0500

    addLetterGrade method added

--- student.py
+++ student.py
@@ -19,3 +19,16 @@ class Student:
     def addGrade(self, gradePoint, credits):
         self.qpoints = self.qpoints + (gradePoint * credits)
         self.credits = self.credits + credits
+
+    def addLetterGrade(self, letterGrade, credits):
+        if letterGrade == 'A':
+            self.qpoints = self.qpoints + (4 * credits)
+        elif letterGrade == 'B':
+            self.qpoints = self.qpoints + (3 * credits)
+        elif letterGrade == 'C':
+            self.qpoints = self.qpoints + (2 * credits)
+        elif letterGrade == 'D':
+            self.qpoints = self.qpoints + (1 * credits)
+        else:
+            self.qpoints = self.qpoints
+        self.credits = self.credits + credits
