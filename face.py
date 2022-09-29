2a2c99ffd6ec2c82ab93f1946b0ce8d127bded56
Mario Vega <malbertovega@gmail.com>
2022-09-29 09:46:28 -0500

    Face class created, to be edited

---
+++ face.py
@@ -0,0 +1,23 @@
+from graphics import *
+
+class Face:
+
+    def __init__(self, window, center, size):
+        eyeSize = 0.15 * size
+        eyeOff = size / 3.0
+        mouthSize = 0.8 * size
+        mouthOff = size / 2.0
+        self.head = Circle(center, size)
+        self.head.draw(window)
+        self.leftEye = Circle(center, eyeSize)
+        self.leftEye.move(-eyeOff, -eyeOff)
+        self.rightEye = Circle(center, eyeSize)
+        self.rightEye.move(eyeOff, -eyeOff)
+        self.leftEye.draw(window)
+        self.rightEye.draw(window)
+        p1 = center.clone()
+        p1.move(-mouthSize/2, mouthOff)
+        p2 = center.clone()
+        p2.move(mouthSize/2, mouthOff)
+        self.mouth = Line(p1,p2)
+        self.mouth.draw(window)
