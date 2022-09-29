b8a956eee14d15030f590c9065703f7cea786428
Mario Vega <malbertovega@gmail.com>
2022-09-29 11:05:06 -0500

    testing smile method from Face class

--- drawFaces13.py
+++ drawFaces13.py
@@ -4,22 +4,23 @@ from button import Button
 
 def main():
     win = GraphWin('Faces', 500, 400)
-    win.setCoords(0,40,50,0)
-    Face(win, Point(17,20), 12)
-    grim_button = Button(win, Point(40, 7.5), 8, 2.5, 'Grim')
+    face = Face(win, Point(170,200), 120)
+    grim_button = Button(win, Point(400, 75), 80, 25, 'Grim')
     grim_button.activate()
-    smile_button = Button(win, Point(40, 12.5), 8, 2.5, 'Smile')
+    smile_button = Button(win, Point(400, 125), 80, 25, 'Smile')
     smile_button.activate()
-    wink_button = Button(win, Point(40, 17.5), 8, 2.5, 'Wink')
+    wink_button = Button(win, Point(400, 175), 80, 25, 'Wink')
     wink_button.activate()
-    frown_button = Button(win, Point(40, 22.5), 8, 2.5, 'Frown')
+    frown_button = Button(win, Point(400, 225), 80, 25, 'Frown')
     frown_button.activate()
-    flinch_button = Button(win, Point(40, 27.5), 8, 2.5, 'Flinch')
+    flinch_button = Button(win, Point(400, 275), 80, 25, 'Flinch')
     flinch_button.activate()
-    quit_button = Button(win, Point(40, 32.5), 6, 2.5, 'Quit!')
+    quit_button = Button(win, Point(400, 325), 60, 25, 'Quit!')
     pt = win.getMouse()
     while not quit_button.clicked(pt):
-        quit_button.activate()
+        if smile_button.clicked(pt):
+            face.smile()
+            quit_button.activate()
         pt = win.getMouse()
     win.close()
 
