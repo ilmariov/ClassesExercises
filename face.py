20d951a6196783137bc13e744d32e57d16927167
Mario Vega <malbertovega@gmail.com>
2022-09-30 18:33:34 -0500

    faces are drawn but can't be undrawn

--- face.py
+++ face.py
@@ -23,11 +23,26 @@ class Face:
         p1.move(-self.mouthSize/2, mouthOff)
         p2 = center.clone()
         p2.move(self.mouthSize/2, mouthOff)
-        self.mouth = Line(p1,p2)
-        self.mouth.draw(window)
+        self.grim_mouth = Line(p1,p2)
+        self.grim_mouth.draw(window)
+        # setting face state
+        self.__set_booleans(True, False, False, False, False)
+        self.smile_pts = []
+        self.frown_pts = []
+        self.leftEye_pts = []
+        self.rightEye_pts = []
+
+    def grim(self):
+        self.__undrawing()
+        if not self.booleans[0]:
+            self.grim_mouth.draw(self.window)
+        if self.booleans[3]:
+            self.leftEye.draw(self.window)
+        if self.booleans[4]:
+            self.rightEye.draw(self.window)
 
     def smile(self):
-        self.mouth.undraw()
+        self.__undrawing()
         h = self.center.getX()
         k = self.center.getY() + self.mouthSize/4
         x1 = round(h - self.mouthSize*0.65)
@@ -37,32 +52,18 @@ class Face:
         for i in range(x1,x2+1):
             y = (b * math.sqrt(1 - ((i-h)**2 / a**2))) + k
             Point(i,y).draw(self.window)
+            self.smile_pts.append(Point(i,y))
+        self.__set_booleans(False, True, False, False, False)
 
     def wink(self):
-        self.mouth.undraw()
-        h = self.center.getX()
-        k = self.center.getY() + self.mouthSize/4
-        x1 = round(h - self.mouthSize*0.65)
-        x2 = round(h + self.mouthSize*0.65)
-        a = self.mouthSize*0.65
-        b = self.mouthSize/2
-        for i in range(x1,x2+1):
-            y = (b * math.sqrt(1 - ((i-h)**2 / a**2))) + k
-            Point(i,y).draw(self.window)
-        eye_center = self.leftEye.getCenter()
-        self.leftEye.undraw()
-        h1 = eye_center.getX()
-        k1 = eye_center.getY() + self.eyeSize/4
-        x3 = round(h1 - self.eyeSize*2/3)
-        x4 = round(h1 + self.eyeSize*2/3)
-        a1 = self.eyeSize*2/3
-        b1 = self.eyeSize/5
-        for j in range(x3,x4+1):
-            y1 = -(b1 * math.sqrt(1 - ((j-h1)**2 / a1**2))) + k1
-            Point(j,y1).draw(self.window)
+        self.__undrawing()
+        self.smile()
+        self.__left_wink()
+        self.__set_booleans(False, True, False, True, False)
 
     def frown(self):
-        self.mouth.undraw()
+        self.__undrawing()
+        self.grim_mouth.undraw()
         h = self.center.getX()
         k = self.center.getY() + self.mouthSize*2/3
         x1 = round(h - self.mouthSize*0.65)
@@ -72,37 +73,65 @@ class Face:
         for i in range(x1,x2+1):
             y = -(b * math.sqrt(1 - ((i-h)**2 / a**2))) + k
             Point(i,y).draw(self.window)
+            self.frown_pts.append(Point(i,y))
+        self.__set_booleans(False, False, True, False, False)
     
     def flinch(self):
-        self.mouth.undraw()
-        h = self.center.getX()
-        k = self.center.getY() + self.mouthSize*2/3
-        x1 = round(h - self.mouthSize*0.65)
-        x2 = round(h + self.mouthSize*0.65)
-        a = self.mouthSize*0.65
-        b = self.mouthSize/2
+        self.__undrawing()
+        self.frown()
+        self.__left_wink()
+        self.__right_wink()
+        self.__set_booleans(False, False, True, True, True) 
+
+    def __left_wink(self):
+        eye_center = self.leftEye.getCenter()
+        self.leftEye.undraw()
+        h = eye_center.getX()
+        k = eye_center.getY() + self.eyeSize/4
+        x1 = round(h - self.eyeSize*2/3)
+        x2 = round(h + self.eyeSize*2/3)
+        a = self.eyeSize*2/3
+        b = self.eyeSize/5
         for i in range(x1,x2+1):
             y = -(b * math.sqrt(1 - ((i-h)**2 / a**2))) + k
             Point(i,y).draw(self.window)
-        eye_center1 = self.leftEye.getCenter()
-        self.leftEye.undraw()
-        h1 = eye_center1.getX()
-        k1 = eye_center1.getY() + self.eyeSize/4
-        x3 = round(h1 - self.eyeSize*2/3)
-        x4 = round(h1 + self.eyeSize*2/3)
-        a1 = self.eyeSize*2/3
-        b1 = self.eyeSize/5
-        for j in range(x3,x4+1):
-            y1 = -(b1 * math.sqrt(1 - ((j-h1)**2 / a1**2))) + k1
-            Point(j,y1).draw(self.window)
-        eye_center2 = self.rightEye.getCenter()
+            self.leftEye_pts.append(Point(i,y))
+
+    def __right_wink(self):
+        eye_center = self.rightEye.getCenter()
         self.rightEye.undraw()
-        h2 = eye_center2.getX()
-        k2 = eye_center2.getY() + self.eyeSize/4
-        x5 = round(h2 - self.eyeSize*2/3)
-        x6 = round(h2 + self.eyeSize*2/3)
-        a2 = self.eyeSize*2/3
-        b2 = self.eyeSize/5
-        for m in range(x5,x6+1):
-            y2 = -(b2 * math.sqrt(1 - ((m-h2)**2 / a2**2))) + k2
-            Point(m,y2).draw(self.window)
+        h = eye_center.getX()
+        k = eye_center.getY() + self.eyeSize/4
+        x1 = round(h - self.eyeSize*2/3)
+        x2 = round(h + self.eyeSize*2/3)
+        a = self.eyeSize*2/3
+        b = self.eyeSize/5
+        for i in range(x1,x2+1):
+            y = -(b * math.sqrt(1 - ((i-h)**2 / a**2))) + k
+            Point(i,y).draw(self.window)
+            self.rightEye_pts.append(Point(i,y))
+
+    def __set_booleans(self, bGrim, bSmile, bFrown, b_left_blink, b_right_blink):
+        '''Parameters set booleans'''
+        self.grimming = bGrim
+        self.smiling = bSmile
+        self.frowning = bFrown
+        self.blinking_left = b_left_blink
+        self.blinking_right = b_right_blink
+        self.booleans = [self.grimming, self.smiling, self.frowning, self.blinking_left, 
+            self.blinking_right]
+
+    def __deleteSemiEllipse(self, objectList):
+        for i in objectList:
+            i.undraw()
+
+    def __undrawing(self):
+        self.actions = [self.grim_mouth.undraw(), self.__deleteSemiEllipse(self.smile_pts),
+            self.__deleteSemiEllipse(self.frown_pts), self.__deleteSemiEllipse(self.leftEye_pts),
+            self.__deleteSemiEllipse(self.rightEye_pts)]
+        for i in range(5):
+            curves = [self.smile_pts, self.frown_pts, self.leftEye_pts, self.rightEye_pts]
+            if self.booleans[i]:
+                self.actions[i]
+                if i>0:
+                    curves[i-1] = []
