02b8b28191de7c30851cc885776ddc1eaf457755
Mario Vega <malbertovega@gmail.com>
2022-10-02 21:19:10 -0500

    exercise 17, testing Target class

--- animation15.py
+++ animation15.py
@@ -7,22 +7,24 @@ def main():
     Line(Point(-10,0), Point(210,0)).draw(win)
     for x in range(0, 210, 50):
         Text(Point(x,-5), str(x)).draw(win)
-        Line(Point(x,0), Point(x,2)).draw(win)
-    
+        Line(Point(x,0), Point(x,2)).draw(win)    
     angle, vel, height = 45.0, 40.0, 2.0
-    while True:
+    target = Target(win, 210)
+    hit_target = False
+    while not(hit_target):
         inputwin = InputDialog(angle, vel, height)
         choice = inputwin.interact()
-
         if choice == 'Quit':
             break
-
         angle, vel, height = inputwin.getValues()
         shot = ShotTracker(win, angle, vel, height)
         while 0 <= shot.getY() and 0 <= shot.getX() <= 210:
             shot.update(1/50)
             update(50)
         inputwin.close()
+        if 0 <= shot.getX() <= 210:
+            hit_target = target.hitTarget(shot)
     win.close()
+
 
 if __name__=='__main__':main()
