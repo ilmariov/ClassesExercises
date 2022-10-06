f342c3061f469caadbadc3c1df6d362d7426b458
Mario Vega <malbertovega@gmail.com>
2022-10-05 23:03:45 -0500

    regresion class created

--- regression.py
+++ regression.py
@@ -1,3 +1,5 @@
+from graphics import *
+
 class Regression:
 
     def __init__(self):
@@ -6,8 +8,6 @@ class Regression:
         self.total_y = 0
         self.sum_xy_prod = 0
         self.sum_x_square = 0
-        self.x_avg = self.total_x / self.n
-        self.y_avg = self.total_y / self.n
 
     def addPoint(self, x, y):
         self.n = self.n + 1
@@ -17,14 +17,19 @@ class Regression:
         self.sum_x_square = self.sum_x_square + (x*x)
 
     def predict(self, x):
+        x_avg = self.total_x / self.n
+        y_avg = self.total_y / self.n
         if self.n > 1:
-            m = self.__slope__()
-            y = self.y_avg + (m * (x - self.x_avg))
+            m = self.__slope__(x_avg, y_avg)
+            y = y_avg + (m * (x - x_avg))
             return y
         else:
             return self.total_y
 
-    def __slope__(self):
-        numerator = self.sum_xy_prod - (self.n * self.x_avg * self.y_avg)
-        denominator = self.sum_x_square - (self.n * self.x_avg)
-        return numerator/denominator
+    def getN(self):
+        return self.n
+
+    def __slope__(self, x_avg, y_avg):
+        numerator = self.sum_xy_prod - (self.n * x_avg * y_avg)
+        denominator = self.sum_x_square - (self.n * x_avg)
+        return numerator/denominator  
